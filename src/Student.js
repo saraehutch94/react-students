3ce762ef80cc66f2cc0f6d1886a028fe540b4289
saraehutch94 <84148986+saraehutch94@users.noreply.github.com>
2021-12-30 13:22:10 -0500

    map through scores array / return Score component

--- src/Student.js
+++ src/Student.js
@@ -1,8 +1,13 @@
 function Student(props) {
+  const scores = props.scores.map((object, index) => {
+    return <Score key={index} date={object.date} score={object.score} />;
+  });
+
   return (
     <div>
       <h1>{props.name}</h1>
       <h3>{props.bio}</h3>
+      <p>{scores}</p>
     </div>
   );
 }
